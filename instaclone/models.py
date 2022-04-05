809ccb26e7e5c9a87c9b06be900e3ea3c012a9e0
github_username <jacobrugano@gmail.com>
2022-04-05 10:16:33 +0300

    Added tests

--- instaclone/models.py
+++ instaclone/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Profile(models.Model):
-    # user =models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    user =models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     profile_picture = models.ImageField(upload_to='images/')
     bio = models.TextField(max_length=500)
     name = models.CharField( max_length=120)
@@ -33,5 +33,6 @@ class Post(models.Model):
     profile = models.ForeignKey('Profile', on_delete = models.CASCADE,default='',related_name='user_profile')
     caption = models.CharField(max_length=250)
     name = models.CharField(max_length=250, default='')
-    created_date = models.DateTimeField(default=timezone.now)
+    # created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
--- instaclone/tests.py
+++ instaclone/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile,Post
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Profile(user=1,profile_picture='image',bio='jacob',name='jacobm',location='kenya',email='jacobrugano@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user,Profile))
