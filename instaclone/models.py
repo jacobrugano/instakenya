c691f24ceb1fd42a6c05e027874da101523a953e
github_username <jacobrugano@gmail.com>
2022-04-05 08:20:17 +0300

    Created Profile modelnd made the migrations

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/')
+    bio = models.TextField(max_length=500)
+    name = models.CharField( max_length=120)
+    location = models.CharField(max_length=60)
+    
